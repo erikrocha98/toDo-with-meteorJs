ccc3333df4d8304fb5e876f50c49242cef802404
erikrocha98 <146686859+erikrocha98@users.noreply.github.com>
2025-03-11 15:03:15 -0300

    feat: Refatoração do componente task

--- todo-react/imports/ui/task.jsx
+++ todo-react/imports/ui/task.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Avatar, IconButton, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
+import EditIcon from '@mui/icons-material/Edit';
+import Checkbox from '@mui/material';
 
 export const Task = ({ task, onCheckBoxClick, onDeleteClick }) => {
   return (
-    <li>
+    <ListItem
+      secondaryAction={
+        <>
+          <IconButton 
+            edge="end" 
+            aria-label="edit" 
+            sx={{ mr: "10px" }}
+          >
+            <EditIcon />
+          </IconButton>
+
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={() => onDeleteClick(task)}
+          >
+            <DeleteIcon />
+          </IconButton>
+          
+        </>
+      }
+    >
+      <ListItemAvatar>
+        <Avatar>
+          <FolderIcon />
+        </Avatar>
+      </ListItemAvatar>
+      <Checkbox
+        checked={!!task.isChecked}
+        onClick={() => onCheckBoxClick(task)}
+      />
+      <ListItemText primary={task.text} secondary="Texto Secundário" />
+    </ListItem>
+    /* <li>
       <input 
         type='checkBox' 
         onClick={()=>onCheckBoxClick(task)} 
@@ -12,6 +50,6 @@ export const Task = ({ task, onCheckBoxClick, onDeleteClick }) => {
       <span>{task.text}</span>
       <button onClick={()=>onDeleteClick(task)}>&times;</button>
 
-    </li>
+    </li> */
   );
 };
